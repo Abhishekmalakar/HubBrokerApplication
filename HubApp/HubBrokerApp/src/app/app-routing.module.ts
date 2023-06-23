52ddb9b34d4f93925044591b19e941685145290c
Abhishekmalakar <malakarabhishek1998@gmail.com>
2023-06-23 13:21:31 +0530

    Update AngularRoutingModel

--- HubApp/HubBrokerApp/src/app/app-routing.module.ts
+++ HubApp/HubBrokerApp/src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StudentRegistationComponent } from './student-registation/student-registation.component';
 import { AdminSideComponent } from './admin-side/admin-side.component';
-import { DocumentsFileComponent } from './documents-file/documents-file.component';
+
 
 const routes: Routes = [
   {path:'',redirectTo:'/registration',pathMatch:'full'},
   { path:'registration',component:StudentRegistationComponent },
  {path:'admin-side',component:AdminSideComponent},
- {path:'documents-file',component:DocumentsFileComponent}
+
 ];
 
 @NgModule({
